e6992a648b3730df1ed1eab712840692e76ec118
Sergey Melekhin <sergey@melekhin.me>
2025-03-14 16:12:29 +0700

    remove commented-out code

--- tests/interface_type.go
+++ tests/interface_type.go
@@ -9,18 +9,3 @@ type WrapperType struct {
 type InterfaceType struct {
 	Field1 int `json:"Field1"`
 }
-
-// type EmbeddedInnerType2 struct {
-// 	Field3 int
-// }
-
-// var embeddedTypeValue EmbeddedType
-
-// func init() {
-// 	embeddedTypeValue.Field1 = 1
-// 	embeddedTypeValue.Field2 = 2
-// 	embeddedTypeValue.Inner.Field1 = 3
-// 	embeddedTypeValue.Field3 = 4
-// }
-
-// var embeddedTypeValueString = `{"Inner":{"Field1":3},"Field2":2,"named":{"Field3":4},"Field1":1}`
